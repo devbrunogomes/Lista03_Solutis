4abc05e77b54af20d786d886c68faaf144c1f247
Bruno Gomes <bsgomes16@gmail.com>
2024-08-19 10:39:09 -0300

    test commit in new branch

---
+++ src/main/java/solutis/squad1_lista3/Questao29.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package solutis.squad1_lista3;
+
+/**
+ *
+ * @author bsgom
+ */
+public class Questao29 {
+    public static void resposta() {
+        System.out.println("Resposta");
+    }
+}
